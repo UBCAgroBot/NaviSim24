d72ca7086b34df46036e1c4791b659549bc4371a
CihanBosnali <cihanbosnali@gmail.com>
2024-02-11 01:08:39 -0800

    Add crop detection

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <cmath>
 #include <chrono>
+#include <opencv2/imgproc.hpp>
 #include <thread>
 #include <iostream>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 #define SHOW_FRAMES 1
 #define FRAMES_PER_SECOND 24
@@ -38,13 +40,27 @@ class Robot {
 			this->y += (speed / FRAMES_PER_SECOND) * std::sin(this->angle * 3.1415/180);
 		}
 
-		void detect(Crop* crops[]) {
-		
+		std::vector<Crop> detect(Crop crops[]) {
+			std::vector<Crop> detected;
+			double center_x = (int) this->x + (ROBOT_HEIGHT + 20) * std::cos((this->angle) * 3.1415/180);
+			double center_y = (int) this->y + (ROBOT_HEIGHT + 20) * std::sin((this->angle) * 3.1415/180);
+
+			for (int i = 0; i < 300; i++)
+			{
+				if (std::sqrt((crops[i].x - center_x)*(crops[i].x - center_x) + (crops[i].y - center_y)*(crops[i].y - center_y)) < 70)
+				{
+					detected.push_back(crops[i]);
+				}
+			}
+
+			return detected;
 		}
 };
 
 void draw_robot(cv::Mat* img, Robot* r)
 {
+	cv::circle(*img, cv::Point((int) r->x + (ROBOT_HEIGHT + 20) * std::cos((r->angle) * 3.1415/180), (int) r->y + (ROBOT_HEIGHT + 20) * std::sin((r->angle) * 3.1415/180)), 70, cv::Scalar(20, 20, 20), cv::FILLED, cv::LINE_8);
+	
 	double x1 = r->x + (ROBOT_WIDTH / 2) * std::cos((r->angle+90) * 3.1415/180) + (ROBOT_HEIGHT / 2) * std::cos((r->angle) * 3.1415/180);	
 	double y1 = r->y + (ROBOT_WIDTH / 2) * std::sin((r->angle+90) * 3.1415/180) + (ROBOT_HEIGHT / 2) * std::sin((r->angle) * 3.1415/180);
 
@@ -62,6 +78,9 @@ void draw_robot(cv::Mat* img, Robot* r)
 	cv::line(*img,cv::Point(x3,y3),cv::Point(x4,y4),cv::Scalar(255,0,0),2);
 	cv::line(*img,cv::Point(x3,y3),cv::Point(x1,y1),cv::Scalar(255,0,0),2);
 	cv::circle(*img, cv::Point((int) r->x + (ROBOT_HEIGHT / 2) * std::cos((r->angle) * 3.1415/180), (int) r->y + (ROBOT_HEIGHT / 2) * std::sin((r->angle) * 3.1415/180)), 5, cv::Scalar( 0, 0, 255), cv::FILLED, cv::LINE_8);
+
+
+
 }
 
 int main(int argc, char* argv[])
@@ -92,7 +111,7 @@ int main(int argc, char* argv[])
 	while (1)
 	{
 		frame_number++;
-		//robot.move(100, 0.2);
+		robot.move(100, -0.2);
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000 / FRAMES_PER_SECOND));
 
 		// show frame 
@@ -101,6 +120,7 @@ int main(int argc, char* argv[])
 		draw_robot(&img, &robot);
 
 		std::cout << "NaviSim: FRAME " << frame_number << std::endl;
+
 		for (Crop c : crops) 
 		{
 			cv::circle(img,
@@ -110,6 +130,18 @@ int main(int argc, char* argv[])
       	cv::FILLED,
       	cv::LINE_8 );	
 		}
+
+		std::vector<Crop> detected = robot.detect(crops);
+		for (Crop c : detected) 
+		{
+			cv::circle(img,
+      	cv::Point((int) c.x, (int) c.y),
+      	c.r,
+      	cv::Scalar( 0, 0, 255),
+      	cv::FILLED,
+      	cv::LINE_8 );	
+		}
+
 
 		cv::imshow("NaviSim", img);
 		cv::waitKey(1);
